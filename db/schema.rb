86aeb52613b3179d3df042577a528767a9d0e52b
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-13 11:39:51 -0600

    associated categories with products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113172502) do
+ActiveRecord::Schema.define(:version => 20131113172916) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20131113172502) do
     t.decimal  "price"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
 end
